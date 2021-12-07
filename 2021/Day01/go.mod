c919070dc0244086640c97e152661e0140111c4c
François Delbrayelle <fdelbrayelle@gmail.com>
2021-12-07 21:39:17 +0100

    Advent of Code 2021 - Day 01 - Part 1 - OK

---
+++ 2021/Day01/go.mod
@@ -0,0 +1,3 @@
+module adventofcode2021/day01
+
+go 1.17
---
+++ 2021/Day01/part1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	content, err := ioutil.ReadFile("input")
+	if err != nil {
+		fmt.Println("ioutil.ReadFile Err")
+	}
+	lines := strings.Split(string(content), "\n")
+	nums := make([]int, 0, len(lines))
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println("strconv.Atoi Err")
+		}
+		nums = append(nums, n)
+	}
+	var increasedCount int
+	for i := 0; i < len(nums); i++ {
+		if i != 0 && nums[i-1] < nums[i] {
+			increasedCount++
+		}
+	}
+	fmt.Println(increasedCount)
+}
