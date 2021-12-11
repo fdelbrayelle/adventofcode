67fb30141e7ef423664586569c3436eeeb26890d
François Delbrayelle <fdelbrayelle@gmail.com>
2021-12-11 21:00:38 +0100

    Advent of Code 2021 - Day 04 - Part 1 - WIP

---
+++ 2021/Day04/day04.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type Board struct {
+	content [][]int
+}
+
+func main() {
+	content, err := ioutil.ReadFile("input")
+	if err != nil {
+		fmt.Println("ioutil.ReadFile Err")
+	}
+	lines := strings.Split(string(content), "\n\n")
+	// numbers := strings.Split(lines[0], ",")
+	makeBoard(lines)
+}
+
+func makeBoard(lines []string) []Board {
+	lines = lines[1:]
+	var boards []Board
+	board := Board{}
+	board.content = make([][]int, 5)
+	for lineIdx := 0; lineIdx < len(lines); lineIdx++ {
+		if len(lines[lineIdx]) == 0 {
+			continue
+		}
+		boardLine := strings.Split(lines[lineIdx], "\n")
+		for i := 0; i < len(boardLine); i++ {
+			rows := strings.Fields(boardLine[i])
+			numRows := len(rows)
+			if numRows != 0 {
+				board.content[i%numRows] = make([]int, numRows)
+				for j := 0; j < numRows; j++ {
+					board.content[i%numRows][j], _ = strconv.Atoi(rows[j])
+				}
+				if i%numRows == numRows-1 {
+					boards = append(boards, board)
+					board = Board{}
+					board.content = make([][]int, numRows)
+				}
+			}
+		}
+	}
+	return boards
+}
---
+++ 2021/Day04/go.mod
@@ -0,0 +1,3 @@
+module adventofcode2021/day04
+
+go 1.17
