76361ec4a2a81605d85a85d2de0b9724c88d6cfe
François Delbrayelle <fdelbrayelle@gmail.com>
2021-12-24 14:27:05 +0100

    Advent of Code 2021 - Day 08 - Part 2 - WIP

--- 2021/Day08/day08.go
+++ 2021/Day08/day08.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"reflect"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -12,19 +15,21 @@ func main() {
 		fmt.Println("ioutil.ReadFile Err")
 	}
 	inputLines := strings.Split(string(content), "\n")
-	outputValues := getOutputValues(inputLines)
+	_, outputValues := getValues(inputLines)
 	fmt.Println("Part 1 result is:", getDigitCount(outputValues, 1)+getDigitCount(outputValues, 4)+getDigitCount(outputValues, 7)+getDigitCount(outputValues, 8))
 }
 
-func getOutputValues(inputLines []string) []string {
+func getValues(inputLines []string) ([]string, []string) {
+	inputValues := make([]string, len(inputLines)-1)
 	outputValues := make([]string, len(inputLines)-1)
 	for i := 0; i < len(inputLines); i++ {
 		values := strings.Split(string(inputLines[i]), " | ")
 		if i != len(inputLines)-1 {
+			inputLines[i] = values[0]
 			outputValues[i] = values[1]
 		}
 	}
-	return outputValues
+	return inputValues, outputValues
 }
 
 /*
@@ -47,7 +52,7 @@ b    .  b    .  .    c  b    c  b    c
  gggg    gggg    ....    gggg    gggg
 */
 
-func getDigits() map[int][]string {
+func getDigitsLettersPart1() map[int][]string {
 	digits := make(map[int][]string, 10)
 	digits[0] = []string{"a", "b", "c", "e", "f", "g"}      // 6
 	digits[1] = []string{"c", "f"}                          // 2 OK
@@ -57,14 +62,14 @@ func getDigits() map[int][]string {
 	digits[5] = []string{"a", "b", "d", "f", "g"}           // 5
 	digits[6] = []string{"a", "b", "d", "e", "f", "g"}      // 6
 	digits[7] = []string{"a", "c", "f"}                     // 3 OK
-	digits[8] = []string{"a", "b", "c", "d", "e", "f", "g"} // 8 OK
+	digits[8] = []string{"a", "b", "c", "d", "e", "f", "g"} // 7 OK
 	digits[9] = []string{"a", "b", "c", "d", "f", "g"}      // 6
 	return digits
 }
 
 func getDigitCount(outputLines []string, digit int) int {
 	count := 0
-	segmentsNumber := len(getDigits()[digit])
+	segmentsNumber := len(getDigitsLettersPart1()[digit])
 	for _, outputLine := range outputLines {
 		numbers := strings.Split(string(outputLine), " ")
 		for _, number := range numbers {
@@ -74,4 +79,61 @@ func getDigitCount(outputLines []string, digit int) int {
 		}
 	}
 	return count
+}
+
+/*
+ dddd
+e    a
+e    a
+ ffff
+g    b
+g    b
+ cccc
+*/
+
+func getDigitsLettersPart2() map[int][]string {
+	digits := make(map[int][]string, 10)
+	digits[0] = []string{"a", "b", "c", "d", "e", "g"}      // 6
+	digits[1] = []string{"a", "b"}                          // 2 OK
+	digits[2] = []string{"a", "c", "d", "f", "g"}           // 5
+	digits[3] = []string{"a", "b", "c", "d", "f"}           // 5
+	digits[4] = []string{"a", "b", "e", "f"}                // 4 OK
+	digits[5] = []string{"b", "c", "d", "e", "f"}           // 5
+	digits[6] = []string{"b", "c", "d", "e", "f", "g"}      // 6
+	digits[7] = []string{"a", "b", "d"}                     // 3 OK
+	digits[8] = []string{"a", "b", "c", "d", "e", "f", "g"} // 7 OK
+	digits[9] = []string{"a", "b", "c", "d", "e", "f"}      // 6
+	return digits
+}
+
+func decodeLine(outputLine string) (int, error) {
+	result := ""
+	numbers := strings.Split(string(outputLine), " ")
+	for _, number := range numbers {
+		for i := 0; i <= 9; i++ {
+			digitLetters := getDigitsLettersPart1()[i]
+			if len(number) == len(digitLetters) {
+				numberLetters := strings.Split(string(number), "")
+				sort.Slice(numberLetters, func(i, j int) bool {
+					return numberLetters[i] < numberLetters[j]
+				})
+				sort.Slice(digitLetters, func(i, j int) bool {
+					return digitLetters[i] < digitLetters[j]
+				})
+				if reflect.DeepEqual(numberLetters, digitLetters) {
+					result += strconv.Itoa(i)
+				}
+			}
+		}
+	}
+	return strconv.Atoi(result)
+}
+
+func decodeLines(outputLines []string) int {
+	sum := 0
+	for _, outputLine := range outputLines {
+		result, _ := decodeLine(outputLine)
+		sum += result
+	}
+	return sum
 }
--- 2021/Day08/day08_test.go
+++ 2021/Day08/day08_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestGetDigit1Count(t *testing.T) {
+func TestGetDigitCounts(t *testing.T) {
 	// Given
 	outputValues := []string{"fdgacbe cefdb cefbgd gcbe", "fcgedb cgb dgebacf gc", "cg cg fdcagb cbg", "efabcd cedba gadfec cb", "gecf egdcabf bgf bfgea", "gebdcfa ecba ca fadegcb", "cefg dcbef fcge gbcadfe", "ed bcgafe cdgba cbgef", "gbdfcae bgc cg cgb", "fgae cfgab fg bagce"}
 	expectedDigitsCount := 26
@@ -16,5 +16,35 @@ func TestGetDigit1Count(t *testing.T) {
 	if actualDigitsCount != expectedDigitsCount {
 		t.Errorf("Digit 1 count was incorrect:\nactual  : %v,\nexpected: %v",
 			actualDigitsCount, expectedDigitsCount)
+	}
+}
+
+func TestDecodeLine(t *testing.T) {
+	// Given
+	outputLine := "cdfeb fcadb cdfeb cdbaf"
+	expectedResult := 5353
+
+	// When
+	var actualResult, _ = decodeLine(outputLine)
+
+	// Then
+	if actualResult != expectedResult {
+		t.Errorf("Result was incorrect:\nactual  : %v,\nexpected: %v",
+			actualResult, expectedResult)
+	}
+}
+
+func TestDecodeLines(t *testing.T) {
+	// Given
+	outputLines := []string{"fdgacbe cefdb cefbgd gcbe", "fcgedb cgb dgebacf gc", "cg cg fdcagb cbg", "efabcd cedba gadfec cb", "gecf egdcabf bgf bfgea", "gebdcfa ecba ca fadegcb", "cefg dcbef fcge gbcadfe", "ed bcgafe cdgba cbgef", "gbdfcae bgc cg cgb", "fgae cfgab fg bagce"}
+	expectedResult := 61229
+
+	// When
+	var actualResult = decodeLines(outputLines)
+
+	// Then
+	if actualResult != expectedResult {
+		t.Errorf("Result was incorrect:\nactual  : %v,\nexpected: %v",
+			actualResult, expectedResult)
 	}
 }
